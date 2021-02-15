6c0b542402d6b1e944208a99f2aaf2de3066a661
lanxuexing <lanxuexing313wsr@163.com>
2021-02-16 00:03:17 +0800

    style: 优化结构

--- src/views/home/childComps/HomeSwiper.vue
+++ src/views/home/childComps/HomeSwiper.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-    <van-swipe v-if="banners?.length" class="swipe" :autoplay="3000" indicator-color="white">
-      <van-swipe-item v-for="banner in banners" :key="banner.id">
-        <img v-lazy="banner.img_url" :alt="banner.title">
-      </van-swipe-item>
-    </van-swipe>
-  </div>
+  <van-swipe v-if="banners?.length" class="swipe" :autoplay="3000" indicator-color="white">
+    <van-swipe-item v-for="banner in banners" :key="banner.id">
+      <img v-lazy="banner.img_url" :alt="banner.title">
+    </van-swipe-item>
+  </van-swipe>
 </template>
 
 <script>
